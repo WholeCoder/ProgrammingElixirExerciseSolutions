0be74ec53b5198a8f14ba364d1b1c91e7b366c7f
MathGeek78 <rpierich@hotmail.com>
2014-09-16 16:43:23 -0400

    Added code that needs to be modified to exercise nodes 3.  Note...must update this code!

--- exercises_nodes_3.exs
+++ exercises_nodes_3.exs
@@ -5,3 +5,51 @@
 # client, and so on). Once the last client receives a tick, it 
 # starts back at the first. The solution should deal with new clients 
 # being added at any time.
+
+defmodule Ticker do
+
+  @interval 2000   # 2 seconds
+
+  @name  :ticker
+
+  def start do
+    pid = spawn(__MODULE__, :generator, [[]])
+    :global.register_name(@name, pid)
+  end
+
+  def register(client_pid) do
+    send :global.whereis_name(@name), { :register, client_pid }
+  end
+
+  def generator(clients) do
+    receive do
+      { :register, pid } ->
+        IO.puts "registering #{inspect pid}"
+        generator([pid|clients])
+
+    after
+      @interval ->
+        IO.puts "tick"
+        Enum.each clients, fn client ->
+          send client, { :tick }
+        end
+        generator(clients)
+    end
+  end
+end
+
+defmodule Client do
+
+  def start do
+    pid = spawn(__MODULE__, :receiver, [])
+    Ticker.register(pid)
+  end
+
+  def receiver do
+    receive do
+      { :tick } ->
+        IO.puts "tock in client"
+        receiver
+    end
+  end
+end

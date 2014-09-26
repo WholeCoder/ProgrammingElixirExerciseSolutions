6ccd26138b7d51d85ce8bacc97c03f7352453fd9
MathGeek78 <rpierich@hotmail.com>
2014-09-25 22:40:42 -0400

    Added coder for exercises 3,4,and 5 for otp chapter

--- exercise_otpservers_1_and_2_3_and_4_5/lib/server.ex
+++ exercise_otpservers_1_and_2_3_and_4_5/lib/server.ex
@@ -13,12 +13,32 @@
 defmodule Stack.Server do
 	use GenServer
 
+	def start_link(current_stack) do
+		GenServer.start_link(__MODULE__, current_stack, name: __MODULE__)
+	end
+
+	def pop do
+		GenServer.call __MODULE__, :pop
+	end
+	
+	def push(element) do
+		GenServer.cast __MODULE__, {:push, element}
+	end
+	
 	def handle_call(:pop, _from, [head | rest]) do
 		{ :reply, head, rest}
 	end
 
 	def handle_cast({:push, element}, current_stack) do
+		if element < 10 do
+			System.halt(-1)
+		end
 		{ :noreply, [element | current_stack]}
+	end
+	
+	def terminate(reason, state) do
+		IO.puts "terminated Stack.Server - reason == #{inspect reason}"
+		IO.puts "		state == #{inspect state}"
 	end
 	
 end
--- test_case_bug.exs
+++ test_case_bug.exs
@@ -1,6 +1,7 @@
 func = fn -> 0 end
-
-case func.() do
-	0 -> IO.puts "Zero!"
-	1 -> IO.puts "One"
+var = 15
+case true do
+	rem(var,5)+rem(var,3) == 0 -> IO.puts "5 and 3"
+	rem(var,5) == 0  -> IO.puts "five"
+	rem(var,3) == 0  -> IO.puts "three"
 end
